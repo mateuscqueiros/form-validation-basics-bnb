d975e4001980a71b0f5ad76ce24c32a956e3e4b1
Mateus Queirós <mateuscqueiros@gmail.com>
2024-05-14 09:44:58 -0300

    Usando defaultValue e labels

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,45 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState('Mateus');
+  const [value, setValue] = useState(0);
+
   return (
     <form
+      className="flex flex-col gap-5 w-[300px]"
       onSubmit={(e) => {
         e.preventDefault();
         console.log({
           name,
+          value,
         });
       }}
     >
-      <input
-        type="text"
-        name="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <label className="flex flex-col items-start">
+        <span>Nome</span>
+        <input
+          type="text"
+          name="name"
+          defaultValue="Mateus"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label className="flex flex-col items-start">
+        <span>Valor</span>
+        <input
+          type="number"
+          name="value"
+          defaultValue={0}
+          onChange={(e) => setValue(Number(e.target.value))}
+        />
+      </label>
+      <label className="flex flex-col items-start">
+        <span>Marca</span>
+        <select name="brand" onChange={(e) => setValue(Number(e.target.value))}>
+          <option value="samsung">Samsung</option>
+          <option value="apple">Apple</option>
+          <option value="xiaomi">Xiaomi</option>
+        </select>
+      </label>
       <button type="submit">Enviar</button>
     </form>
   );
